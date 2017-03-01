487124443730c38d41d9e092d1968fcbf7e6136d
raysonli <raysonli@t8277.wpa.dal.ca>
2017-03-01 14:25:25 -0400

    a3-stage2 create 3 more rules for validating email

--- src/main/java/email_validator/EmailValidator.java
+++ src/main/java/email_validator/EmailValidator.java
@@ -6,20 +6,30 @@ public class EmailValidator {
 			
 			int count = 0;
 			int t1 = 0, t2 =0;
-			
 			for(int i = 0; i < email.length(); i++){ //look through the email string
 				
 				if(email.charAt(i) == '@') //if there is @ in the string
 					t1++;
 				if(email.charAt(i) == '.') //if there is . in the string
 					t2++;
-
 			}
 				
 			if(t1 == 1) //if there is one single @ in the string
 				count++;
 			
 			if(t2 >= 1) //if there is at least one . in the string
+				count++;
+/*
+ * own rules:
+ */
+			
+			if(email.matches("^[^<>%$]*$")) //if there is not included special character
+				count++;
+			
+			if(email.matches("^[^\\s]*$")) //if there is not included space
+				count++;
+			
+			if(email.matches("^.*[.com|ca]$")) //if there is end with .com or .ca
 				count++;
 			
 			return count;
