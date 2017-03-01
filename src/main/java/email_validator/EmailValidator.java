fc1cc334dbd19a15e50f36ef8d07a09dbc19c4f1
raysonli <raysonli@t8277.wpa.dal.ca>
2017-03-01 13:28:50 -0400

    a3-stage 1 validate email with given rules

--- src/main/java/email_validator/EmailValidator.java
+++ src/main/java/email_validator/EmailValidator.java
@@ -5,20 +5,23 @@ public class EmailValidator {
 		public int validate(String email){
 			
 			int count = 0;
-			int t1 = 0;
-			int t2 =0;
-			for(int i = 0; i < email.length(); i++){
-				if(email.charAt(i) == '@')
+			int t1 = 0, t2 =0;
+			
+			for(int i = 0; i < email.length(); i++){ //look through the email string
+				
+				if(email.charAt(i) == '@') //if there is @ in the string
 					t1++;
-				if(email.charAt(i) == '.')
+				if(email.charAt(i) == '.') //if there is . in the string
 					t2++;
+
 			}
-			if(t1 == 1){
+				
+			if(t1 == 1) //if there is one single @ in the string
 				count++;
-			}
-			if(t2 >= 1){
+			
+			if(t2 >= 1) //if there is at least one . in the string
 				count++;
-			}
+			
 			return count;
 		}
 }
--- src/test/java/email_validator/ValidateTest.java
+++ src/test/java/email_validator/ValidateTest.java
@@ -6,11 +6,44 @@ import org.junit.Test;
 
 public class ValidateTest {
 
+	//one single @ and at least one . in the email string
 	@Test
-	public void testValidator() {
+	public void testValidator1() {
 		EmailValidator test = new EmailValidator();
 		int result = test.validate("123@dal.ca");
 		assertEquals(2,result);
+	}
+	
+	//no @ and at least one . in the email string
+	@Test
+	public void testValidator2() {
+		EmailValidator test = new EmailValidator();
+		int result = test.validate("123dal..ca");
+		assertEquals(1,result);
+	}
+	
+	//one single @ and no . in the email string
+	@Test
+	public void testValidator3() {
+		EmailValidator test = new EmailValidator();
+		int result = test.validate("123@dalca");
+		assertEquals(1,result);
+	}
+	
+	//more than one @ and at least one . in the email string
+	@Test
+	public void testValidator4() {
+		EmailValidator test = new EmailValidator();
+		int result = test.validate("123@@dal.ca");
+		assertEquals(1,result);
+	}
+	
+	//either @ or no . in the email string
+	@Test
+	public void testValidator5() {
+		EmailValidator test = new EmailValidator();
+		int result = test.validate("123dalca");
+		assertEquals(0,result);
 	}
 
 }
